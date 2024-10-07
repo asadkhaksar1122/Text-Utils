5f4772fa2aaae57b9d3b0c8f685732cfab133c18
Asad Khaksar <119926803+asadkhaksar1122@users.noreply.github.com>
2024-10-07 11:40:25 +0500

    favicon added

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  base: "/Text-Utils/",
   server: {
-    host: '0.0.0.0', // Allows access from network devices
+    host: "0.0.0.0", // Allows access from network devices
     port: 5173, // Default port
   },
-
-})
+});
